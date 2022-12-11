a48b312c1af0b3bf8dcfe9d4c63b946014d81f59
ragingscout95 <prakhar99rajput@gmail.com>
2022-12-11 17:58:04 +0530

    added css

--- src/components/markdown/Markdown_editor.js
+++ src/components/markdown/Markdown_editor.js
@@ -6,10 +6,19 @@ const Markdown_editor = () => {
   const [MarkDown, SetMarkDown] = React.useState("Welcome to Markdown Editor");
   return (
     <div className='main-div'>
-      <textarea className='left-side' value={MarkDown} onChange={(e)=> SetMarkDown(e.target.value)}></textarea>
-      <div className='right-side'>
+      <div className='up'>
+        <div className='btn-nav'>
+          <span className='yel-circle'></span>
+          <span className='gre-circle'></span>
+          <span className='red-circle'></span>
+        </div>
+      </div>
+        <div className='down'>
+        <textarea className='left-side' value={MarkDown} onChange={(e)=> SetMarkDown(e.target.value)}></textarea>
+        <div className='right-side'>
         <ReactMarkdown>{MarkDown}</ReactMarkdown>
         </div>
+      </div>
     </div>
   )
 }
